5f5a2b97b7eb6a652c97a21da8af7694d528b8d9
AARON_BURCIAGA_CORTES <a.21308051280321@cbtis128.edu.mx>
2024-04-24 15:15:27 +0000

    a 2.0

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:disenioburciaga/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/burger.png",
+      title: "Burger",
+      description: "Las mejores hamburguesas de toda la ciudad"),
+  ProductModel(
+      img: "assets/images/dessert.png",
+      title: "Dessert",
+      description: "Los postres mas deliciosos que imagines"),
+  ProductModel(
+      img: "assets/images/pizza.png",
+      title: "Pizza",
+      description: "Las pizzas mas ricas del mundo"),
+];
